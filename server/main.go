00855b7fadb466058e94a2dcbaf051b15b4db9de
wxqdoit <876009857@qq.com>
2020-09-04 18:35:11 +0800

    新功能 ：接口优化，好友关系，群关系

--- server/main.go
+++ server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"goChat/control"
 	"goChat/db"
 	"goChat/util"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -15,6 +17,10 @@ const (
 func handleIntercept(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
+		w.Header().Set("content-type", "application/json")             //返回数据格式是json
+		w.Header().Set("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
 		w.WriteHeader(http.StatusOK)
 		userToken := r.Header.Get("token")
 		//是否有token
@@ -36,12 +42,25 @@ func handleIntercept(h http.HandlerFunc) http.HandlerFunc {
 func ApiInit() {
 	http.HandleFunc("/user/login", control.UserLogin)
 	http.HandleFunc("/user/register", control.UserRegister)
+	http.HandleFunc("/user/test", testDelete)
 	http.HandleFunc("/user/token/check", handleIntercept(control.CheckUserToken))
+}
+
+func testDelete(w http.ResponseWriter, request *http.Request) {
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE")
+
+	req, _ := ioutil.ReadAll(request.Body)
+	fmt.Printf(string(req))
+	_, _ = w.Write(req)
 }
 
 //主函数
 func main() {
-
+	fmt.Println(util.MakePassword("olive888", "0r4p09kxq6cj"))
 	//连接数据库
 	db.InitDb()
 
